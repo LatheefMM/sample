67c1c1e00b08e1e545b1ba26c6e6722660850bc0
ABDUL LATHEEF M.M <latheefmm34@gmail.com>
2023-03-29 15:00:12 +0530

    commit

---
+++ bin/maths.dart
@@ -0,0 +1,17 @@
+import 'dart:io';
+
+void main(){
+   print("enter 2 numbers");
+
+  int a = int.parse(stdin.readLineSync()!);
+  int b = int.parse(stdin.readLineSync()!);
+
+  print("sum = ${a+b}");
+  print("sub = ${a-b}");
+  print("div = ${a/b}");
+  print("mul = ${a*b}");
+
+
+
+
+}
