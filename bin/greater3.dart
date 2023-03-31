b98d6f1a6c87d7e7b49dca3bbbb59b439d21c9e4
ABDUL LATHEEF M.M <latheefmm34@gmail.com>
2023-03-31 23:48:09 +0530

    commit

---
+++ bin/greater3.dart
@@ -0,0 +1,16 @@
+void main(){
+  int a = 30;
+  int b = 22;
+  int c = 33;
+  bool greater = (a>b) && (a>c) ? true:false;
+  print(greater);
+
+  bool great = (b>a) && (b>c)? true:false;
+  print(great);
+
+  bool gre = (c>a) && (c>b);
+  print(gre);
+
+
+
+}
