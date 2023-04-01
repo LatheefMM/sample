4305298dbaf4da55f23a103ed965aeabcf53cd2f
ABDUL LATHEEF M.M <latheefmm34@gmail.com>
2023-04-01 15:25:37 +0530

    commit

--- bin/greater3.dart
+++ bin/greater3.dart
@@ -2,14 +2,10 @@ void main(){
   int a = 30;
   int b = 50;
   int c = 33;
-  bool greater = (a>b) && (a>c) ? true:false;
+  var greater = (a>b&&a>c)?"largest number is 30":(b>c)? "largest number is 50":"largest number is 33";
   print(greater);
 
-  bool great = (b>a) && (b>c)? true:false;
-  print(great);
 
-  bool gre = (c>a) && (c>b);
-  print(gre);
 
 
 
